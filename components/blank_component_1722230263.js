5b7210453e3fa611a33846931703f3166c6ce208
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-29 05:18:38 +0000

    Code generation

--- components/blank_component_1722230263.js
+++ components/blank_component_1722230263.js
@@ -17,24 +17,24 @@ Vue.component("blank_component_1722230263", {
             <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
             <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
             <div class="flex">
-                <div class="w-64 bg-gray-800 h-screen">
+                <div class="w-64 bg-purple-900 h-screen">
                     <nav class="mt-10">
-                        <a href="#" class="flex items-center px-6 py-2 text-gray-100 hover:bg-gray-700">
+                        <a href="#" class="flex items-center px-6 py-2 text-white hover:bg-purple-800 rounded-lg m-2">
                             <i class='bx bx-home mr-3'></i>
-                            <span>Home</span>
+                            <span class="font-sans">Home</span>
                         </a>
-                        <a href="#" class="flex items-center px-6 py-2 mt-4 text-gray-100 hover:bg-gray-700">
+                        <a href="#" class="flex items-center px-6 py-2 mt-4 text-white hover:bg-purple-800 rounded-lg m-2">
                             <i class='bx bx-info-circle mr-3'></i>
-                            <span>About</span>
+                            <span class="font-sans">About</span>
                         </a>
-                        <a href="#" class="flex items-center px-6 py-2 mt-4 text-gray-100 hover:bg-gray-700">
+                        <a href="#" class="flex items-center px-6 py-2 mt-4 text-white hover:bg-purple-800 rounded-lg m-2">
                             <i class='bx bx-envelope mr-3'></i>
-                            <span>Contact</span>
+                            <span class="font-sans">Contact</span>
                         </a>
                     </nav>
                 </div>
                 <div class="flex-1">
-                    <section id="blank-section" class="bg-white dark:bg-gray-900 flex-1">
+                    <section id="blank-section" class="bg-white dark:bg-black flex-1">
                         <div id="blank-container" class="max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
 
                         </div>
